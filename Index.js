9c0237ae088dac807ad9f98f151832f3e55930d8
unknown <yvkanvu@gmail.com>
2023-09-07 12:05:37 +0200

    creation index.html style.css index.js

---
+++ Index.js
@@ -0,0 +1,14 @@
+//Diffence entre let et var
+//LET: est une déclaration de variable qui permet de déclarer une variable dont 
+//la portée est limitée dans lequel elle est déclarée.( une fonction, une boucle ou une instruction conditionnelle)
+//càd si tu déclares une avec "let" à l'interieur d'une fonction, elle ne sera accessible que dans cette fonction.
+//De plus "let" n'autorise pas la réutilisation d'une variable avec le meme bloc.
+//VAR: est une déclaration utilisée dans l'ancienne version JS; elle a une portée de fonction ce qui signifie qu'une  
+// variable déclarée avec "var" est accessible dans toute la fonction dans la quelle elle est déclarée.
+//Contrairement à "let" "var" autorise la réutilisation d'une variable avec le meme bloc, ce qui peut parfois causer
+//des problemes de confusion.
+//il est recommandé d'utiliser "let" plutot que "var" pour déclarer les variables en JS, car cela permet
+//une meilleure gestion de la portée et évite les problemes de réutilisation accidentelle de variables.
+//
+//
+// 
