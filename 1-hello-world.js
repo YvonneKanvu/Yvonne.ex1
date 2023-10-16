153516f3b8ba24a3c5c4bf709d9c9763d0052e38
mbuhendwa <mkbuhendwa@gmail.com>
2023-10-16 17:32:49 +0100

    solution exercice #2

---
+++ 1-hello-world.js
@@ -0,0 +1,5 @@
+// Write a program that prints ‘Hello World’ to the screen.
+// https://adriann.github.io/programming_problems.html
+
+let salutation = "Hello, World";
+console.log(salutation);
---
+++ 2-greeting-user.js
@@ -0,0 +1,8 @@
+// https://adriann.github.io/programming_problems.html
+
+// demander a l'utilisateur son nom
+const nom = prompt("Quel est ton nom ?");
+
+// salue l'utilisateur par son nom
+// console.log(`Salut ${nom} !`); // afficher dans la console
+alert(`Salut ${nom} !`); // afficher dans une boite de dialogue
